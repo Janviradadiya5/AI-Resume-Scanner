a122bfcd07c883e2fb889bec4556cd273d8deb6a
Janvi Radadiya <janviradadiya61@gmail.com>
2025-06-17 12:15:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,7 @@ def dashboard():
         recommendations=recommendations
     )
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+
+# Run Flask app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
